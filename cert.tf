b5750b015a37fa37b283c8f1798cb7bdcc07f6e8
Diego Sogari <diego.sogari@gmail.com>
2023-11-01 20:57:24 -0300

    Add HTTPS listener

--- cert.tf
+++ cert.tf
@@ -26,3 +26,9 @@ resource "namecheap_domain_records" "default" {
     ttl      = 300
   }
 }
+
+resource "aws_acm_certificate" "default" {
+  private_key       = acme_certificate.default.private_key_pem
+  certificate_body  = acme_certificate.default.certificate_pem
+  certificate_chain = acme_certificate.default.issuer_pem
+}
